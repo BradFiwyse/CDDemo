7ff64402ca1b1efac2f43d7d6d5b11557eecb6ac
BradFiwyse <brad.andrews@fiwyse.com>
2026-02-16 13:50:11 -0600

    added data and table

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
 import streamlit as st
+import pandas as pd
 
 st.title("My First Deployed App 🚀")
 st.write("Hello from Streamlit Community Cloud!")
+
+
+with open('data.json') as f:
+    data=json.load(f)
+
+df= pd.Dataframe(data) 
+
+st.dataframe(df)
---
+++ tst.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+df  = pd.read_json('Data.json')
+print(df.head())
