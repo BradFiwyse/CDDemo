8a503d804270b8a36ce9f6ac1242dc96d90d36bf
BradFiwyse <brad.andrews@fiwyse.com>
2026-02-16 20:22:41 -0600

    trying again

--- tests/test_tst.py
+++ tests/test_tst.py
@@ -1,6 +1,11 @@
-# tests/test_tst.py
+import sys
+import os
+
+# Add project root to Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from tst import add
 
+
 def test_add():
-    assert add(2, 3) == 5  # this should pass
-    assert add(0, 0) == 0  # another simple check
+    assert add(2, 3) == 5
--- tst.py
+++ tst.py
@@ -1,4 +1,12 @@
-import pandas as pd
+# import pandas as pd
+ 
 
-df  = pd.read_json('Data.json')
-print(df.head())
+# df  = pd.read_json('Data.json')
+# print(df.head())
+
+# tst.py
+def add(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    print(add(2, 3))
